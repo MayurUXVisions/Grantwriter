70b61dc08aae2eb3d7931718c99044876c26d602
MayurUXVisions <142739165+MayurUXVisions@users.noreply.github.com>
2024-08-15 00:28:20 +0530

    Updated home-hero.php

--- wp-content/themes/grantwriter/template-parts/blocks/home-hero.php
+++ wp-content/themes/grantwriter/template-parts/blocks/home-hero.php
@@ -2,9 +2,9 @@
 	<div class="home-hero__bg" >
 		<div class="home-hero__video">
 			<video loop="true" preload="none" autoplay="autoplay" muted="" playinline="" playsinline="" webkit-playsinline="">
-				<source src="http://localhost/grantwriter/wp-content/uploads/2024/08/gw-homehero.mp4" type="video/mp4">
+				<source src="<?php echo site_url(); ?>/wp-content/uploads/2024/08/gw-homehero.mp4" type="video/mp4">
 			</video>
-		</div>
+		</div> 
 		<div class="container home-hero__flex" data-aos="fade-up">
 			<label>America’s #1 Grant Writers, since 1995</label>
 			<h1>We’re the Difference Between <strong>Success</strong> and Reject</h1>
